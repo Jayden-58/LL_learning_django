afd1505bdfff70c96a32d92cb1dcaf780a904490
Jayden-58 <jayden.kellar@gmail.com>
2022-09-25 17:49:00 -0700

    registered models with admin

--- app/albums/admin.py
+++ app/albums/admin.py
@@ -1,3 +1,8 @@
+from unicodedata import name
 from django.contrib import admin
-
+from .models import Publisher, Artist, Album, Song
 # Register your models here.
+admin.site.register(Publisher)
+admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Song)
--- app/albums/models.py
+++ app/albums/models.py
@@ -1,14 +1,20 @@
-from platform import release
+
 from django.db import models
 
 class Publisher(models.Model):
     name = models.CharField(max_length=40, blank=True)
-    date_founded = models.DateField(blank=True)
+    date_founded = models.CharField(max_length=4,blank=True)
+
+    def __str__(self):
+        return self.name, self.date_founded
 
 class Artist(models.Model):
     first_name = models.CharField(max_length=35)
     last_name = models.CharField(max_length=40, blank=True)
     birthdate = models.DateField(blank=True)
+
+    def __str__(self):
+        return self.first_name, self.last_name, self.birthdate
 
 class Album(models.Model):
     title = models.CharField(max_length=100)
@@ -19,11 +25,17 @@ class Album(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title, self.matrix, self.release_date, self.country_of_pressing, self.artist, self.publisher
+
 class Song(models.Model):
     title = models.CharField(max_length=15)
     length = models.CharField(max_length=15, blank=True) #maybe later I'll try duration field instead?
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title, self.length, self.artist, self.album
     
 
 
