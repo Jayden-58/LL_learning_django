e53b7fddda73ce2908d6f1f2d01cef29b1b6a443
Jayden-58 <jayden.kellar@gmail.com>
2022-09-26 12:34:26 -0700

    added a bit of data

--- app/albums/models.py
+++ app/albums/models.py
@@ -6,36 +6,36 @@ class Publisher(models.Model):
     date_founded = models.CharField(max_length=4,blank=True)
 
     def __str__(self):
-        return self.name, self.date_founded
+        return f'{self.name} {self.date_founded}'
 
 class Artist(models.Model):
     first_name = models.CharField(max_length=35)
     last_name = models.CharField(max_length=40, blank=True)
-    birthdate = models.DateField(blank=True)
+    birthdate = models.DateField(blank =True, null=True)
 
     def __str__(self):
-        return self.first_name, self.last_name, self.birthdate
+        return f'{self.first_name} {self.last_name} {self.birthdate}'
 
 class Album(models.Model):
     title = models.CharField(max_length=100)
     matrix = models.CharField(max_length=20, blank=True)
-    release_date = models.DateField(blank=True)
+    release_date = models.DateField(blank=True, null=True)
     country_of_pressing = models.CharField(max_length=40, blank=True)
-    submission_date = models.DateTimeField()
+    submission_date = models.DateTimeField(blank=True, null=True)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title, self.matrix, self.release_date, self.country_of_pressing, self.artist, self.publisher
+        return f'{self.title} {self.matrix} {self.release_date} {self.country_of_pressing} {self.artist} {self.publisher}'
 
 class Song(models.Model):
-    title = models.CharField(max_length=15)
+    title = models.CharField(max_length=40)
     length = models.CharField(max_length=15, blank=True) #maybe later I'll try duration field instead?
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title, self.length, self.artist, self.album
+        return f'{self.title} {self.length} {self.artist} {self.album}'
     
 
 
