0faee451535f97fed0e8547698643eb42f21e864
Jayden-58 <jayden.kellar@gmail.com>
2022-09-26 12:40:30 -0700

    formatting changes

--- app/albums/models.py
+++ app/albums/models.py
@@ -6,7 +6,7 @@ class Publisher(models.Model):
     date_founded = models.CharField(max_length=4,blank=True)
 
     def __str__(self):
-        return f'{self.name} {self.date_founded}'
+        return f'{self.name}' #{self.date_founded}'
 
 class Artist(models.Model):
     first_name = models.CharField(max_length=35)
@@ -14,7 +14,7 @@ class Artist(models.Model):
     birthdate = models.DateField(blank =True, null=True)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name} {self.birthdate}'
+        return f'{self.first_name} {self.last_name}' #{self.birthdate}'
 
 class Album(models.Model):
     title = models.CharField(max_length=100)
@@ -26,7 +26,7 @@ class Album(models.Model):
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.title} {self.matrix} {self.release_date} {self.country_of_pressing} {self.artist} {self.publisher}'
+        return f'{self.title} --- {self.artist}' #{self.matrix} {self.release_date} {self.country_of_pressing} {self.publisher}'
 
 class Song(models.Model):
     title = models.CharField(max_length=40)
@@ -35,7 +35,7 @@ class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.title} {self.length} {self.artist} {self.album}'
+        return f'{self.title} --- {self.artist}'#{self.length} {self.album}'
     
 
 
