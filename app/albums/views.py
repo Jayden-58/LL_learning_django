7a1df6fd311134459785f538ec3b92617f62d892
Jayden-58 <jayden.kellar@gmail.com>
2022-09-26 11:34:17 -0700

    set up simple veiws and urls for testing

--- app/albums/views.py
+++ app/albums/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    return HttpResponse('<p>home veiw</p>')
+
+def album_detail(request, album_id):
+    return HttpResponse(f'<p>album_detail veiw with id {album_id} </p>')
